f7e3c66807ba135a27fe48b7750311a9063b02fc
Clayton.council <48404713+ClayOwen@users.noreply.github.com>
2019-11-19 16:36:19 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,36 @@ echo "Configuring OSX..."
 
 osascript -e 'tell application "System Preferences" to quit'
 
+
+newUser(){
+
+echo "Enter the user name:"
+read SHORTNAME
+
+echo "Enter a full name for user:"
+read FULLNAME
+
+echo "Enter a password for this user:"
+read -s PASSWORD
+
+
+sudo sysadminctl -addUser $SHORTNAME -fullName "$FULLNAME"  -password $PASSWORD
+
+sudo fdesetup add -usertoadd $SHORTNAME
+
+}
+
+
+
+echo "Do you want create a new user? [Y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Changing the remove Mdm Profile "
+      newUser
+else
+        exit 0
+fi
+
 prefSet(){
 # Computer set to never sleep
  sudo systemsetup -setcomputersleep Never
@@ -82,16 +112,6 @@ else
 fi
 }
 
-
-echo "Do you want to remove Mdm Profile? [Y,n]"
-read input
-if [[ $input == "Y" || $input == "y" ]]; then
-        echo "Changing the remove Mdm Profile "
-        UAMDMCheck & Check
-else
-        echo "Leaving existing MDM profiles"
-fi
-
 Check(){
 if [[ $noinst == "1" ]]; then
   echo "No MDM profile Present..."
@@ -120,19 +140,91 @@ elif [[ $noinst == "0" ]]; then
 fi
 }
 
-appSet(){
-  sudo rm -rif /Applications/Microsoft\ Excel.app /Applications/Microsoft\ OneNote.app /Applications/Microsoft\ Outlook.app /Applications/Microsoft\ PowerPoint.app /Applications/Microsoft\ Silverlight /Applications/Microsoft\ Word.app
-  sudo rm -rif /Applications/Adobe\ Acrobat\ DC /Applications/Adobe\ Bridge\ CC\ 2018 /Applications/Adobe\ Creative\ Cloud /Applications/Adobe\ Illustrator\ CC\ 2018 /Applications/Adobe\ InDesign\ CC\ 2018 /Applications/Adobe\ Lightroom\ CC /Applications/Adobe\ Media\ Encoder\ CC\ 2018 /Applications/Adobe\ Photoshop\ CC\ 2018
-  sudo rm -rif /Applications/Adobe\ After\ Effects\ CC\ 2019 /Applications/Adobe\ Audition\ CC\ 2019 /Applications/Adobe\ Bridge\ CC\ 2019 /Applications/Adobe\ Illustrator\ CC\ 2019 /Applications/Adobe\ InDesign\ CC\ 2019 /Applications/Adobe\ Lightroom\ Classic /Applications/Adobe\ Media\ Encoder\ CC\ 2019 /Applications/Adobe\ Photoshop\ CC\ 2019 /Applications/Adobe\ Premiere\ Pro\ CC\ 2019
-  sudo installer -pkg /Volumes/Conference_setup/Conference/AcroRdrDC_1901220036_MUI.pkg -target /
-  sudo installer -pkg /Volumes/Conference_setup/Conference/Microsoft_office_16.16.19081100_Installer.pkg -target /
-  sudo installer -pkg /Volumes/Conference_setup/Conference/Microsoft_Office_2016_VL_Serializer_2.0.pkg -target /
-  sudo mv "Volumes/Conference/Microsoft Remote Desktop.app" /Applications/
-  sudo mv "/Volumes/Conference/Remote Desktop.app" /Applications/
-  sudo mv "/Volumes/Conference/Atom.app" /Applications/
+echo "Do you want to remove Mdm Profile? [Y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Changing the remove Mdm Profile "
+        UAMDMCheck & Check
+else
+        echo "Leaving existing MDM profiles"
+fi
+
+
+hbcheck(){
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "Updating homebrew recipes"
+	brew update
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+	brew install findutils
+
+# Install Bash 4
+	brew install bash
+
+	PACKAGES=(
+		wget
+    winetricks
+    wine-pypi
+		curl
+		pkg-config
+		python
+    python3
+    mas
+	)
+
+	echo "Installing packages..."
+	brew install ${PACKAGES[@]}
+	echo "Cleaning up..."
+	brew cleanup
+fi
+
 }
 
-echo "Do you want to install custom applications? [Y,n]"
+
+
+echo "Do you want to install Homebrew? [Y,N]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing Homebrew.."
+        hbcheck
+else
+        echo "Leaving existing settings..."
+fi
+
+
+appSet(){
+  cd /Applications
+  sudo rm -rif /Applications/Microsoft\ Excel.app /Applications/Microsoft\ OneNote.app /Applications/Microsoft\ Outlook.app /Applications/Microsoft\ PowerPoint.app /Applications/Microsoft\ Silverlight /Applications/Microsoft\ Word.app
+  ## sudo rm -rif /Applications/Adobe\ Acrobat\ DC /Applications/Adobe\ Bridge\ CC\ 2018 /Applications/Adobe\ Creative\ Cloud /Applications/Adobe\ Illustrator\ CC\ 2018 /Applications/Adobe\ InDesign\ CC\ 2018 /Applications/Adobe\ Lightroom\ CC /Applications/Adobe\ Media\ Encoder\ CC\ 2018 /Applications/Adobe\ Photoshop\ CC\ 2018
+  ## sudo rm -rif /Applications/Adobe\ After\ Effects\ CC\ 2019 /Applications/Adobe\ Audition\ CC\ 2019 /Applications/Adobe\ Bridge\ CC\ 2019 /Applications/Adobe\ Illustrator\ CC\ 2019 /Applications/Adobe\ InDesign\ CC\ 2019 /Applications/Adobe\ Lightroom\ Classic /Applications/Adobe\ Media\ Encoder\ CC\ 2019 /Applications/Adobe\ Photoshop\ CC\ 2019 /Applications/Adobe\ Premiere\ Pro\ CC\ 2019
+
+  curl -o /Applications/AcroRdrDC_1902120049_MUI.dmg https://ardownload2.adobe.com/pub/adobe/reader/mac/AcrobatDC/1902120049/AcroRdrDC_1902120049_MUI.dmg
+  hdiutil attach /Applications/AcroRdrDC_1902120049_MUI.dmg
+  sudo installer -pkg /Volumes/AcroRdrDC_1902120049_MUI/AcroRdrDC_1902120049_MUI.pkg -target /
+  sudo rm -rif /Applications/AcroRdrDC_1902120049_MUI.dmg
+  hdiutil detach /Volumes/AcroRdrDC_1902120049_MUI
+
+  curl -o /Applications/Microsoft_Office_16.31.19111002_Installer.pkg https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_16.31.19111002_Installer.pkg
+  sudo installer -pkg /Applications/Microsoft_Offsice_16.31.19111002_Installer.pkg -target /
+  sudo rm -rif /Applications/Microsoft_Office_16.31.19111002_Installer.pkg
+
+  curl -o /applications/Microsoft_Office_2019_VL_Serializer.pkg https://gist.githubusercontent.com/zthxxx/9ddc171d00df98cbf8b4b0d8469ce90a/raw/Microsoft_Office_2019_VL_Serializer.pkg
+  sudo installer -pkg /Applications/Microsoft_Office_2019_VL_Serializer.pkg -target /
+  #sudo rm -rif /Applications/Microsoft_Office_2019_VL_Serializer.pkg
+
+  curl -o /Applications/atom-mac.zip  https://atom-installer.github.com/v1.41.0/atom-mac.zip?s=1571754162&ext=.zip && sudo unzip /Applications/atom-mac.zip
+
+  #sudo rm -rif /Applications/atom-mac.zip
+
+  #curl -o /Applications/Remote+Desktop.app.zip  http://www.mediafire.com/folder/q6om3ndwj0bds/PKG/Remote_Desktop.app.zip/file
+  #sudo unzip /Applications/Remote+Desktop.app.zip
+  #sudo rm -rif /Applications/Remote+Desktop.app.zip http://www.mediafire.com/folder/q6om3ndwj0bds/PKG
+
+}
+
+echo "Do you want to install custom applications? [Y,N]"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
         echo "Installing custom applications"
@@ -143,40 +235,9 @@ fi
 
 
 
-echo "Starting bootstrapping"
-
 # ask for admin password up front
 # sudo -v
 
-# Check for Homebrew, install if we don't have it
-if test ! $(which brew); then
-    echo "Installing homebrew..."
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
 
-echo "Updating homebrew recipes"
-brew update
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
-
-# Install Bash 4
-brew install bash
-
-PACKAGES=(
-	wget
-	wine
-  winetricks
-	curl
-	pkg-config
-	python
-  python3
-  mas
-)
-
-echo "Installing packages..."
-brew install ${PACKAGES[@]}
-
-echo "Cleaning up..."
-brew cleanup
 
 osascript -e 'tell app "System Events" to log out'
